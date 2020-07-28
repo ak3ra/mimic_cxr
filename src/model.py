00a2dcaeeb6c27e16c389006e78046c27b393cdb
Benjamin Akera <akeraben@gmail.com>
2020-07-28 17:52:45 -0400

    Imported torch functional API

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import torchvision
 from torch.utils.data import DataLoader
 from data import PneumoniaDataset
