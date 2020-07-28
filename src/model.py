8805dc31158c151f162024a0c15b16ac0f941ef5
Benjamin Akera <akeraben@gmail.com>
2020-07-28 17:48:09 -0400

    created custom model

--- src/model.py
+++ src/model.py
@@ -11,6 +11,43 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 
+## Custom Model
+class CNN(nn.Module):
+    def __init__(self, in_channel=1, num_classes=2):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels = 1,
+            out_channels = 8,
+            kernel_size = (3,3),
+            strid=(1,1),
+            padding = (1,1)
+        )
+
+        self.pool = nn.MaxPool2d(kernel_size =  (2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(
+            in_channels = 8,
+            out_channels = 16,
+            kernel_size = (3,3),
+            stride = (1,1),
+            padding = (1,1)
+
+        )
+
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = reshape(x.shape[0],-1)
+        x = self.fc1(x)
+
+        return x
+
+
+
 my_transforms = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((512,512)),
@@ -37,9 +74,7 @@ train_loader = DataLoader(dataset=train_set,batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_set,batch_size=batch_size, shuffle=True)
 
 
-# Model
-model = torchvision.models.googlenet(pretrained=True)
-model.to(device)
+model = CNN().to(device)
 
 #loss and Optimizer
 criterion = nn.CrossEntropyLoss()
